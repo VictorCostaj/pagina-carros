9eb01b0f03fe3378e2b043ec87d67262a1994f69
Victor Costa <victorjosueldacosta@gmail.com>
2023-07-20 01:33:11 -0300

    add-responsividade

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ let search = document.querySelector(".search-box");
 document.querySelector("#search-icon").onclick = () => {
   search.classList.toggle("active");
 };
+//header
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+
