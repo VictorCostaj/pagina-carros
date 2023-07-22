2e050c2d4828c40805bbe90b3d9091595580bfd4
Victor Costa <victorjosueldacosta@gmail.com>
2023-07-21 22:52:47 -0300

    finised-responsiv

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-let search = document.querySelector(".search-box");
+const search = document.querySelector(".search-box");
+const menu = document.querySelector(".navbar");
+const header = document.querySelector("header");
 
-document.querySelector("#search-icon").onclick = () => {
+function toggleSearch() {
   search.classList.toggle("active");
-};
-//header
-let header = document.querySelector("header");
+  menu.classList.remove("active");
+}
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
+function toggleMenu() {
+  menu.classList.toggle("active");
+  search.classList.remove("active");
+}
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#menu-icon").addEventListener("click", toggleMenu);
 });
 
+function toggleHeader() {
+  header.classList.toggle("shadow", window.scrollY > 0);
+}
 
+document.querySelector("#search-icon").addEventListener("click", toggleSearch);
+document.querySelector("#menu-icon").addEventListener("click", toggleMenu);
+window.addEventListener("scroll", toggleHeader);
+
+window.onscroll = () => {
+  menu.classList.toggle("active");
+  search.classList.remove("active");
+};
